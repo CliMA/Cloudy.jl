f0696c2ce4866a35b076fae3cd8fa1a01b4ff1c9
edejong-caltech <edejong@caltech.edu>
2023-07-12 10:55:49 -0700

    Nicer distribution plotting

--- examples/two_particles_golovin_moments.jl
+++ examples/two_particles_golovin_moments.jl
@@ -31,6 +31,7 @@ function rhs!(ddist_moments, dist_moments, p, t)
      for i in 1:length(dist_moments[1,:])
          dist_params[:,i] = params_from_moments(dist_moments[:,i])
      end
+     @show dist_params
      # Evaluate processes at inducing points using a closure distribution
      pdists = map(1:p.Ndist) do i
          GammaParticleDistribution(dist_params[1, i], dist_params[2,i], dist_params[3,i])
@@ -44,9 +45,6 @@ function rhs!(ddist_moments, dist_moments, p, t)
              @show dist_params
              error("failure in integrals")
          end
-        #  @show p.Q 
-        #  @show p.R 
-        #  @show p.S
          for k in 1:length(pdists)
             for j in 1:k-1
                 p.coal_ints[m,k] += p.Q[j,k]
@@ -58,10 +56,7 @@ function rhs!(ddist_moments, dist_moments, p, t)
             if k > 1
                 p.coal_ints[m,k] += p.S[k-1, 2]
             end
-            #  p.coal_ints[m,j] += p.S[j,1] - p.R[j,j] - p.R[j,j+1]
-            #  p.coal_ints[m,j+1] += p.S[j,2] + p.Q[j,j+1] + p.Q[j+1,j] + p.Q[j+1,j+1] - p.R[j+1,j] - p.R[j+1,j+1]
-         end
-         #@show p.coal_ints[m,:]
+        end
          ddist_moments[m,:] = p.coal_ints[m,:]
 
          if any(isnan.(ddist_moments[m,:]))
@@ -74,11 +69,11 @@ function rhs!(ddist_moments, dist_moments, p, t)
              error("nan ddt")
          end
      end
-     @show ddist_moments[2,:]
-    #  if ddist_moments[1, 1] > 0.0
-    #      @show (p.Q, p.R, p.S)
-    #      error("positive dn1")
-    #  end
+     #@show ddist_moments
+     if ddist_moments[1, 1] > 0.0
+         @show (p.Q, p.R, p.S)
+         error("positive dn1")
+     end
 end
 
 function main()
@@ -90,13 +85,13 @@ function main()
     # Physical parameters
     # Time has been rescaled below by a factor of 1e1 so that
     # 1 sec = 10 deciseconds
-    time_scale = 1e1
-    mass_scale = 1.0 #0.33e-9
+    time_scale = 1.0
+    mass_scale = 0.5
 
-    T_end = 0.0 * time_scale
-    coalescence_coeff = 1e-4 / time_scale # 1.5e3 cm^3 g^-1 s-1
-    kernel_func = LinearKernelFunction(coalescence_coeff)
-    #kernel_func = ConstantKernelFunction(1e-4 / time_scale)
+    T_end = 1.0 * time_scale
+    coalescence_coeff = 1.0
+    #kernel_func = LinearKernelFunction(coalescence_coeff)
+    kernel_func = ConstantKernelFunction(coalescence_coeff)
 
     # Parameter transform used to transform native distribution
     # parameters to moments and back
@@ -104,10 +99,10 @@ function main()
 
     # Initial condition
     Ndist = 2
-    particle_number = [1e4, 1e2]
-    mean_particles_mass = [mass_scale, 10 * mass_scale]
-    particle_mass_std = [mass_scale/4, 5 * mass_scale]
-    params_init = reduce(vcat, transpose.([particle_number, (mean_particles_mass ./ particle_mass_std).^2, particle_mass_std.^2 ./ mean_particles_mass]))
+    particle_number = [1, 1e-6]
+    particle_k = [2, 2]
+    particle_θ = [mass_scale, 10*mass_scale]
+    params_init = reduce(vcat, transpose.([particle_number, particle_k, particle_θ]))
     dist_moments_init = similar(params_init)
     for i in 1:length(params_init[1,:])
         dist_moments_init[:,i] = moments_from_params(params_init[:,i])
@@ -149,102 +144,152 @@ function main()
     end
     @show params
 
-    p1 = plot(time,
-        moment_0[1,:],
-        linewidth=2,
-        xaxis="time [s]",
-        yaxis="M0 [1/cm^3]",
-        xlims=(0, maximum(time)),
-        ylims=(0, 1.5*maximum(moment_0)),
-        label="M_{0,1}"
-    )
-    plot!(p1, time,
-        moment_0[2,:],
-        linewidth=2,
-        label="M_{0,2}")
-    plot!(p1, time,
-        sum(moment_0, dims=1)[:],
-        linewidth=3,
-        label="M_{0,1+2}")
+    # p1 = plot(time,
+    #     moment_0[1,:],
+    #     linewidth=2,
+    #     xaxis="time [s]",
+    #     yaxis="M0 [1/cm^3]",
+    #     xlims=(0, maximum(time)),
+    #     ylims=(0, 1.5*maximum(moment_0)),
+    #     label="M_{0,1}"
+    # )
+    # plot!(p1, time,
+    #     moment_0[2,:],
+    #     linewidth=2,
+    #     label="M_{0,2}")
+    # plot!(p1, time,
+    #     sum(moment_0, dims=1)[:],
+    #     linewidth=3,
+    #     label="M_{0,1+2}")
 
     p2 = plot(time,
         moment_1[1,:],
         linewidth=2,
-        xaxis="time [s]",
-        yaxis="M1",
+        xaxis="dimensionless time",
+        yaxis="M1 (mass)",
         xlims=(0, maximum(time)),
         ylims=(0, 1.5*maximum(moment_1)),
-        label="M_{1,1}"
+        lc=:blue,
+        label="Subdistribution 1"
     )
     plot!(p2, time,
         moment_1[2,:],
         linewidth=2,
-        label="M_{1,2}")
+        lc=:orange,
+        label="Subdistribution 2")
     plot!(p2, time,
         sum(moment_1, dims=1)[:],
         linewidth=3,
-        label="M_{1,1+2}")
+        lc=:green,
+        label="Sum")
 
-    p3 = plot(time,
-        moment_2[1,:],
-        linewidth=2,
-        xaxis="time [s]",
-        yaxis="M2 [1/cm^3]",
-        xlims=(0, maximum(time)),
-        ylims=(0, 1.5*maximum(moment_2)),
-        label="M_{2,1}"
-    )
-    plot!(p3, time,
-        moment_2[2,:],
-        linewidth=2,
-        label="M_{2,2}")
-    plot!(p3, time,
-        sum(moment_2, dims=1)[:],
-        linewidth=3,
-        label="M_{2,1+2}")
+    # p3 = plot(time,
+    #     moment_2[1,:],
+    #     linewidth=2,
+    #     xaxis="time [s]",
+    #     yaxis="M2 [1/cm^3]",
+    #     xlims=(0, maximum(time)),
+    #     ylims=(0, 1.5*maximum(moment_2)),
+    #     label="M_{2,1}"
+    # )
+    # plot!(p3, time,
+    #     moment_2[2,:],
+    #     linewidth=2,
+    #     label="M_{2,2}")
+    # plot!(p3, time,
+    #     sum(moment_2, dims=1)[:],
+    #     linewidth=3,
+    #     label="M_{2,1+2}")
 
     # plot spectra
-    x = 10 .^ (collect(range(-1, 3, 100)))
+    r = 10 .^ (collect(range(-0.5, 0.5, 100)))
+    x = 4/3 * r.^ 3
     pdists_init = map(1:p.Ndist) do i
         GammaParticleDistribution(params[1, i, 1], params[1, i, 2], params[1, i, 3])
     end
     pdists_fin = map(1:p.Ndist) do i
         GammaParticleDistribution(params[end, i, 1], params[end, i, 2], params[end, i, 3])
     end
-    p4 = plot(x,
-        pdists_init[1].(x),
+    pnew = plot(r,
+        3 * x .* pdists_init[1].(x),
         linewidth=2,
-        xaxis=:log,
-        yaxis="N [1/cm^3]",
-        xlims=(minimum(x), maximum(x)),
-        label="N_{init,1}")
-    plot!(p4, x,
-        pdists_init[2].(x),
+        xscale=:log10,
+        xaxis="dimensionless particle size",
+        yaxis="marginal mass distribution",
+        xlims=(minimum(r), maximum(r)),
+        ls=:dash,
+        lc=:blue,
+        label="")
+    plot!(pnew,
+        r,
+        3 * x .* pdists_init[2].(x),
         linewidth=2,
-        label="N_{init,2}")
-    plot!(p4, x,
-        pdists_init[1].(x) + pdists_init[2].(x),
+        ls=:dash,
+        lc=:orange,
+        label="")
+    plot!(pnew,
+        r,
+        3 * x .* pdists_init[2].(x) .+ 3 * x .* pdists_init[1].(x),
         linewidth=3,
-        label="N_{init}")
-
-    p5 = plot(x,
-        pdists_fin[1].(x).*x,
+        ls=:dash,
+        lc=:green,
+        label="Initial")
+    plot!(pnew,
+        r,
+        3 * x .* pdists_fin[1].(x),
         linewidth=2,
-        xaxis=:log,
-        yaxis="N [1/cm^3]",
-        xlims=(minimum(x), maximum(x)),
-        label="N_{fin,1}")
-    plot!(p5, x,
-        pdists_fin[2].(x).*x,
+        lc=:blue,
+        ls=:solid,
+        label="")
+    plot!(pnew,
+        r,
+        3 * x .* pdists_fin[2].(x),
         linewidth=2,
-        label="N_{fin,2}")
-    plot!(p5, x,
-        pdists_fin[1].(x).*x + pdists_fin[2].(x).*x,
+        lc=:orange,
+        ls=:solid,
+        label="")
+    plot!(pnew,
+        r,
+        3 * x .* pdists_fin[2].(x) .+ 3 * x .* pdists_fin[1].(x),
         linewidth=3,
-        label="N_{fin}")
+        ls=:solid,
+        lc=:green,
+        label="Final")
+    # p4 = plot(x,
+    #     pdists_init[1].(x),
+    #     linewidth=2,
+    #     xaxis=:log,
+    #     yaxis="N [1/cm^3]",
+    #     xlims=(minimum(x), maximum(x)),
+    #     label="N_{init,1}")
+    # plot!(p4, x,
+    #     pdists_init[2].(x),
+    #     linewidth=2,
+    #     label="N_{init,2}")
+    # plot!(p4, x,
+    #     pdists_init[1].(x) + pdists_init[2].(x),
+    #     linewidth=3,
+    #     label="N_{init}")
 
-    plot(p1, p2, p3, p4, p5, layout=(2, 3), size=(1200, 700), margin=5Plots.mm)
-    savefig("test.png")
+    # p5 = plot(x,
+    #     pdists_fin[1].(x).*x,
+    #     linewidth=2,
+    #     xaxis=:log,
+    #     yaxis="N [1/cm^3]",
+    #     xlims=(minimum(x), maximum(x)),
+    #     label="N_{fin,1}")
+    # plot!(p5, x,
+    #     pdists_fin[2].(x).*x,
+    #     linewidth=2,
+    #     label="N_{fin,2}")
+    # plot!(p5, x,
+    #     pdists_fin[1].(x).*x + pdists_fin[2].(x).*x,
+    #     linewidth=3,
+    #     label="N_{fin}")
+
+    #plot(p1, p2, p3, p4, p5, layout=(2, 3), size=(1200, 700), margin=5Plots.mm)
+    plot(p2, pnew, layout=(1,2), size=(700, 300), margin=5Plots.mm)
+    savefig("test2.pdf")
 end
 
 #@code_warntype main()
