26d96a5eb6511940c8f5b35b5b2ad36a9dccad7e
Emily de Jong <edejong@caltech.edu>
2024-01-09 10:46:30 -0800

    Remove hybrid coalescence style

--- src/Sources/EquationTypes.jl
+++ src/Sources/EquationTypes.jl
@@ -8,12 +8,10 @@ export AbstractStyle
 export CoalescenceStyle
 export AnalyticalCoalStyle
 export NumericalCoalStyle
-export HybridCoalStyle # TODO
 
 abstract type AbstractStyle end
 abstract type CoalescenceStyle <: AbstractStyle end
 struct NumericalCoalStyle <: CoalescenceStyle end
 struct AnalyticalCoalStyle <: CoalescenceStyle end
-struct HybridCoalStyle <: CoalescenceStyle end # TODO: implement
 
 end #module EquationTypes.jl
